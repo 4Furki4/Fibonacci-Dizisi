bfd05bb4a9b825c995c39ee307eca8fa1ec1d184
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-03 23:09:26 +0300

    Konsol Mesajı güncellendi

--- Konsol_Islemleri.cs
+++ Konsol_Islemleri.cs
@@ -3,6 +3,14 @@ namespace fibo
 {
     public class Konsol_Islemleri
     {
-        
+        public void KonsolMesajı()
+        {
+            Console.WriteLine("Lütfen fibonacci dizisi için integer değerinde bir genişlik giriniz.");
+        }
+        public int DiziGenisligiAlma()
+        {
+            int genislik=int.Parse(Console.ReadLine());
+            return genislik;
+        }
     }
 }
