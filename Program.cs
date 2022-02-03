67e857a69c7bcb28f6853608a87d788de877dc2a
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-03 23:18:49 +0300

    Kurucu metotlar çalıştırılıp metotlara erişilerek metotları çalıştıracak kodlar ve yorum satırları yazıldı

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace fibo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Konsol_Islemleri Konsol = new Konsol_Islemleri();
+            fibonaccIslemleri fibonacci = new fibonaccIslemleri(); // Constructor metotlar çalıştırıldı.
+            Konsol.KonsolMesajı(); // Konsola mesaj yazdıran metot çalıştırıldı.
+            int genislik= Konsol.DiziGenisligiAlma(); // Dizinin genişliğini alıp döndüren metot çalıştırıldı ve genislik adlı değişkene atandı.
+            int[] fibonacciDizisi= fibonacci.FibonacciDizisiOluşturma(genislik); //Fibonacci dizisini oluşturan metot çalıştırıldı ve yeni bir diziye atandı.
+            int ortalama= fibonacci.FibDizisininOrtaması(fibonacciDizisi); // Fib dizisinin ortalamasını alan metot çalıştırıldı ve dönen değer yeni bir değişkene atandı.
+            Konsol.OrtalamayıYazdırma(ortalama);
         }
     }
 }
