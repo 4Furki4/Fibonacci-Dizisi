96c13b08cb321d75ddd595243bd8e17940664637
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-03 22:59:28 +0300

    fibonaccIslemleri sınıfında FibonacciDizisiOlusturma adlı metotla int paramtre ile fib dizisinin genişliğini alıp oluşturdum, bu diziyi döndürdüm ve FibDizisininOrtaması adlı metotta int dizi parametresiyle bu diziyi kullanarak ortalamasını döndürdüm.

--- fibonaccIslemleri.cs
+++ fibonaccIslemleri.cs
@@ -3,6 +3,25 @@ namespace fibo
 {
     public class fibonaccIslemleri
     {
-        
+        public int[] FibonacciDizisiOluşturma(int genislik)
+        {
+            int[] FibDizisi= new int[genislik];
+            FibDizisi[0]=1;
+            FibDizisi[1]=1;
+            for (int i = 2; i <= FibDizisi.Length-1; i++)
+            {
+                FibDizisi[i]=FibDizisi[i-1]+FibDizisi[i-2];
+            }
+            return FibDizisi;
+        }
+        public int FibDizisininOrtaması(int[] ar)
+        {
+            int toplam=0;
+            foreach (var item in ar)
+            {
+                toplam+=item;
+            }
+            return toplam/ar.Length
+        }
     }
 }
